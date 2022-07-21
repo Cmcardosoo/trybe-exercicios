3f4ee313a549348c2346dacbab6f1ec1a75f2ce2
Cmcardosoo <c.marceloo@me.com>
2022-07-21 16:13:29 -0300

    Check 1 Completo

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-1/script.js
@@ -0,0 +1,17 @@
+const a = 3;
+const b = 4;
+
+adicao = a + b;
+console.log(adicao);
+
+subtracao = a - b;
+console.log(subtracao);
+
+multiplicacao = a * b;
+console.log(multiplicacao);
+
+divisao = a / b;
+console.log(divisao);
+
+modulo = a % b;
+console.log(modulo);
