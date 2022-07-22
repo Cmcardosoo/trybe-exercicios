189fc9e5c1c5b2978dec94e4feb07bff4a2b44a5
Cmcardosoo <c.marceloo@me.com>
2022-07-21 21:03:22 -0300

    Check 3 Completo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-1/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-1/script.js
@@ -16,11 +16,24 @@
 // modulo = a % b;
 // console.log(modulo);
 
-const a = 4;
-const b = 2;
+// const a = 4;
+// const b = 2;
 
-if (a > b) {
+// if (a > b) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+// }
+
+const a = 5;
+const b = 6;
+const c = 1;
+
+if ((a > b) & (a > c)) {
   console.log(a);
-} else {
+}
+if ((b > a) & (b > c)) {
   console.log(b);
+} else {
+  console.log(c);
 }
