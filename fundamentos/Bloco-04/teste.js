ca32541a89d8be865bf6900fb1a2254d49e5c4f0
Cmcardosoo <c.marceloo@me.com>
2022-08-02 20:55:42 -0300

    Adicionando Arquivos

--- fundamentos/Bloco-04/teste.js
+++ fundamentos/Bloco-04/teste.js
@@ -1,5 +1,15 @@
-const lado1 = 5;
-let lado2 = 8;
-const lado3 = 3;
-const lado4 = 2;
-const perimiter = lado1 + lado2 + lado3 + lado4;
+//   const lado1 = 5;
+//  let lado2 = 8;
+// const lado3 = 3;
+// const lado4 = 2;
+// const perimiter = lado1 + lado2 + lado3 + lado4;
+
+
+
+const candidate = 30
+
+if (if candidate >= 80) {
+
+    console
+    
+}
