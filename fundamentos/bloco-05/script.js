de267a7161e6f674b03dca2cc25df1dbc8f5a602
Cmcardosoo <c.marceloo@me.com>
2022-08-03 10:29:04 -0300

    Exercicio completo

---
+++ fundamentos/bloco-05/script.js
@@ -0,0 +1,8 @@
+document.getElementById("header-container").style.backgroundColor = "green";
+document.getElementsByTagName("section")[0].style.backgroundColor = "salmon";
+document.getElementsByTagName("h3")[0].style.backgroundColor = "indigo";
+document.getElementsByTagName("h3")[1].style.backgroundColor = "indigo";
+document.getElementsByTagName("section")[1].style.backgroundColor = "gold";
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
+document.getElementsByTagName("div")[4].style.backgroundColor = "DarkSlateGray";
